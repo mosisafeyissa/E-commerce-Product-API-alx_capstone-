68730ede2702fbf7248151004a58f8bcb6b5c4cc
Mosisa Feyissa <mosisafaye@gmail.com>
2025-08-18 11:00:28 +0300

    Simple_Ecommerce_API_Basics

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from categories.models import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
